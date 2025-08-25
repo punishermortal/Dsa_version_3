9f124adf011e9727aa8d5c2f9cac857c000e006c
punishermortal <51474998+punishermortal@users.noreply.github.com>
2025-08-25 21:09:49 +0530

    Time: 188 ms (18.18%), Space: 18.1 MB (39.39%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -9,14 +9,15 @@ class Solution:
         ans=0
         counter=0
         for val in sorted(hm.values(), reverse=True):
-            if counter<8:
-                ans+=val
-            elif(counter>=8 and counter<16):
-                ans+=(val*2)
-            elif(counter>=16 and counter<24):
-                ans+=(val*3)
-            else:
-                ans+=(val*4)
+            # if counter<8:
+            #     ans+=val
+            # elif(counter>=8 and counter<16):
+            #     ans+=(val*2)
+            # elif(counter>=16 and counter<24):
+            #     ans+=(val*3)
+            # else:
+            #     ans+=(val*4)
+            ans+=(val*(counter//8 +1))
             counter+=1
         return ans
             
